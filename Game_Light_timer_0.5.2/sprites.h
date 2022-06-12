5e790fc10e1eedf92cb523d5aa36f9fc489bad2e
Fabian Grosso <93215442+LED-BOY@users.noreply.github.com>
2022-06-11 21:24:25 -0300

    Add files via upload

---
+++ Game_Light_timer_0.5.2/sprites.h
@@ -0,0 +1,42 @@
+/*
+    This file is part of Game&Light Timer
+
+    Foobar is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Foobar is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Foobar.  If not, see <https://www.gnu.org/licenses/>.
+
+    To contact us: ledboy.net
+    ledboyconsole@gmail.com
+*/
+// sandClock
+const  uint8_t sandClock [] = { 0x00, 0xc6, 0xaa, 0x92, 0x92, 0xaa, 0xc6, 0x00 
+};
+
+const  uint8_t battIcon[] = {
+  0x3f, 0x21, 0x21, 0x21, 0x21, 0x21, 0x33, 0x1e
+};
+
+const  uint8_t tempSimbolC [] = {
+  0x07, 0x05, 0x07, 0x00, 0x7c, 0x82, 0x82, 0x44
+};
+
+const  uint8_t tempSimbolF [] = {
+  0x0e, 0x0a, 0x0e, 0x00, 0xfc, 0x14, 0x14, 0x04
+};
+
+const  uint8_t ledSprite [] = {
+  0xf8, 0x80, 0xbe, 0x2a, 0x00, 0x1f, 0x11, 0x0e
+};
+
+const  uint8_t sleepIcon [] = {
+  0xc4, 0xa4, 0x94, 0x8c, 0x00, 0x19, 0x15, 0x13
+};
